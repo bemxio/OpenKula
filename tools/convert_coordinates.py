8c922b21ba17c53d5bd0533056d5ce9ef4daec40
bemxio <bemxiov@protonmail.com>
2026-01-07 20:06:21 +0100

    rewrite script for converting coordinates

--- tools/convert_coordinates.py
+++ tools/convert_coordinates.py
@@ -1,33 +1,39 @@
+import json
 from argparse import ArgumentParser
+from pathlib import Path
 
-def convert_coordinates(x: int, y: int, width: int, height: int) -> tuple[tuple[int, int], tuple[int, int]]:
-    """
-    I gotta be honest, Scratch's coordinate system is kind of confusing, to say the least.
-    For whatever reason, Kula's origin seems to differ from the standard Scratch sprite origin by a little bit, requiring the use of silly offsets.
-    Hence, two sets of coordinates are returned: one for normal use, and one specifically for the player sprite.
-    I might just be the dumb one here, however this function does return an approximate position that works well enough.
-    """
+WIDTH = 480
+HEIGHT = 360
 
-    return (
-        (
-            x + 240 - width // 2, 
-            180 - y - height // 2
-        ),
-        (
-            x + 240 - width + width // 4,
-            180 - y - height // 2 + height // 4
-        )
-    )
+def convert_coordinates(project: Path, sprite: str, x: int, y: int) -> None:
+    with open(project, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    rc_x, rc_y = 0, 0
+
+    for target in data["targets"]:
+        for costume in target["costumes"]:
+            if not costume["name"] == sprite:
+                continue
+
+            rc_x = costume["rotationCenterX"]
+            rc_y = costume["rotationCenterY"]
+
+            break
+
+    new_x = x + WIDTH // 2 - rc_x
+    new_y = HEIGHT // 2 - y - rc_y
+
+    print(f"Original coordinates: ({x}, {y})")
+    print(f"Rotation center: ({rc_x}, {rc_y})")
+    print(f"Converted coordinates: ({new_x}, {new_y})")
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Convert Scratch coordinates to SDL coordinates.")
 
-    parser.add_argument("x", type=int, help="X coordinate in Scratch's coordinate system.")
-    parser.add_argument("y", type=int, help="Y coordinate in Scratch's coordinate system.")
-    parser.add_argument("width", type=int, help="Width of the sprite.")
-    parser.add_argument("height", type=int, help="Height of the sprite.")
+    parser.add_argument("project", type=Path, help="Path to the Scratch's project.json file.")
+    parser.add_argument("sprite", type=str, help="Name of the sprite to convert coordinates for.")
+    parser.add_argument("x", type=int, help="X position in Scratch's coordinate system.")
+    parser.add_argument("y", type=int, help="Y position in Scratch's coordinate system.")
 
-    (x1, y1), (x2, y2) = convert_coordinates(**vars(parser.parse_args()))
-
-    print(f"Normal coordinates: ({x1}, {y1})")
-    print(f"Kula coordinates: ({x2}, {y2})")
+    convert_coordinates(**vars(parser.parse_args()))
