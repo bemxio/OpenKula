ff7f34aa4f6ae96411ac60a4e6a9506f3fdf7984
bemxio <bemxiov@protonmail.com>
2026-01-08 18:03:42 +0100

    move RenderScore function to utils.h

--- kula/src/main.c
+++ kula/src/main.c
@@ -10,24 +10,6 @@
 #include "structs.h"
 #include "utils.h"
 
-void RenderScore(SDL_Renderer* renderer, TTF_Font* font, int32_t score) {
-    SDL_Rect rect = SCORE_POSITION;
-    SDL_Color color = SCORE_COLOR;
-
-    char scoreText[16];
-
-    snprintf(scoreText, sizeof(scoreText), "score: %d", score);
-    TTF_SizeText(font, scoreText, &rect.w, &rect.h);
-
-    SDL_Surface* surface = TTF_RenderText_Solid(font, scoreText, color);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
-}
-
 void GameLogic(GameState* state) {
     if (!state->player.glideState.active) {
         if (state->controls & 1 << 0) {
--- kula/src/utils.c
+++ kula/src/utils.c
@@ -29,3 +29,21 @@ void UpdateGlide(Entity* entity) {
         entity->rect.y = (int32_t)(entity->glideState.startY + (entity->glideState.targetY - entity->glideState.startY) * t);
     }
 }
+
+void RenderScore(SDL_Renderer* renderer, TTF_Font* font, int32_t score) {
+    SDL_Rect rect = SCORE_POSITION;
+    SDL_Color color = SCORE_COLOR;
+
+    char scoreText[16];
+
+    snprintf(scoreText, sizeof(scoreText), "score: %d", score);
+    TTF_SizeText(font, scoreText, &rect.w, &rect.h);
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, scoreText, color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+}
