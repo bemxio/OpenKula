ff9dca2079ad8b83d56144cde5af68149265bda6
bemxio <bemxiov@protonmail.com>
2026-01-08 19:32:09 +0100

    add assets for kula 3d in asset copying script

--- tools/copy_assets_to_game.py
+++ tools/copy_assets_to_game.py
@@ -6,14 +6,21 @@ import xml.etree.ElementTree as ET
 
 LOOKUP_TABLE = {
     "kula": {
-        "2fe2f71497a79460dd88fb83987c29b6.svg": "kulabakgrunn.png",
+        "aeaf1f511fa2bef67371ed62528686df.svg": "fiende01.png",
         "5fc0d3bc8029acfb10700893fc7b7733.svg": "fiende02.png",
         "8a9336e1b924b53fb0d70634706a3392.svg": "kula.png",
+        "2fe2f71497a79460dd88fb83987c29b6.svg": "kulabakgrunn.png",
         "077b5dbb78e1bc63ae11b483d3f5b20f.mp3": "mortietunes.mp3",
-        "aeaf1f511fa2bef67371ed62528686df.svg": "fiende01.png",
         "skrifttype.ttf": "skrifttype.ttf"
     },
-    "kula3D": {},
+    "kula3D": {
+        "2dbac4b426800a196082eb5b7c3a7538.svg": "fiende01.png",
+        "cd58cacba669ecd6177243b1cfbeaf34.svg": "fiende02.png",
+        "d06adeb3a40b10f666861146e668179c.svg": "kula.png",
+        "3dfdfd9f379a15b83ff175401117592d.svg": "kulabakgrunn.png",
+        "077b5dbb78e1bc63ae11b483d3f5b20f.mp3": "mortietunes.mp3",
+        "skrifttype.ttf": "skrifttype.ttf"
+    },
     "kulatwo": {}
 }
 
@@ -37,6 +44,8 @@ def copy_assets_to_game(game: str, directory: str) -> None:
         os.mkdir(f"{game}/assets")
 
     for source, target in LOOKUP_TABLE[game].items():
+        #print(f"{source} -> {target}")
+
         if source.endswith(".svg"):
             extract_png_from_svg(f"assets/{source}", f"{game}/assets/{target}")
         else:
