2e79bb087572d67e74bf1c1c1aab4faba26e4bf2
bemxio <bemxiov@protonmail.com>
2026-01-19 23:46:00 +0100

    render game inside texture for proper scaling on wii in kula

--- kula/src/main.c
+++ kula/src/main.c
@@ -118,8 +118,15 @@ int main(int argc, char* argv[]) {
     TTF_Init();
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GAME_WIDTH, GAME_HEIGHT, SDL_WINDOW_RESIZABLE);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    #ifdef __wii__
+        SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        SDL_Texture* target = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_TARGET, GAME_WIDTH, GAME_HEIGHT);
+    #else
+        SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GAME_WIDTH, GAME_HEIGHT, SDL_WINDOW_RESIZABLE);
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    #endif
+
     SDL_GameController* controller = NULL;
     SDL_Event event;
 
@@ -165,7 +172,6 @@ int main(int argc, char* argv[]) {
 
         .controls = 0
     };
-
     GameAssets assets = {
         .background = IMG_LoadTexture(renderer, BACKGROUND_PATH),
         .music = Mix_LoadMUS(BGM_PATH),
@@ -176,7 +182,10 @@ int main(int argc, char* argv[]) {
         .enemyClosed = IMG_LoadTexture(renderer, ENEMY_CLOSED_PATH)
     };
 
-    SDL_RenderSetLogicalSize(renderer, GAME_WIDTH, GAME_HEIGHT);
+    #ifndef __wii__
+        SDL_RenderSetLogicalSize(renderer, GAME_WIDTH, GAME_HEIGHT);
+    #endif
+
     Mix_PlayMusic(assets.music, -1);
 
     while (loop) {
@@ -257,8 +266,17 @@ int main(int argc, char* argv[]) {
 
         SDL_RenderClear(renderer);
 
+        #ifdef __wii__
+            SDL_SetRenderTarget(renderer, target);
+        #endif
+
         GameLogic(&state);
         GameRender(renderer, &state, &assets);
+
+        #ifdef __wii__
+            SDL_SetRenderTarget(renderer, NULL);
+            SDL_RenderCopy(renderer, target, NULL, NULL);
+        #endif
 
         SDL_RenderPresent(renderer);
         SDL_Delay(1000 / FPS);
@@ -270,6 +288,10 @@ int main(int argc, char* argv[]) {
     SDL_DestroyTexture(assets.player);
     SDL_DestroyTexture(assets.enemyOpen);
     SDL_DestroyTexture(assets.enemyClosed);
+
+    #ifdef __wii__
+        SDL_DestroyTexture(target);
+    #endif
 
     if (controller != NULL) SDL_GameControllerClose(controller);
     SDL_DestroyRenderer(renderer);
