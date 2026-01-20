c9dd006be5ae77bb54e180ca1ec15dcc8f0ae388
bemxio <bemxiov@protonmail.com>
2026-01-20 18:23:31 +0100

    define alternative text on game over on wii in kula 3d

--- kula3D/src/constants.h
+++ kula3D/src/constants.h
@@ -36,6 +36,11 @@
 #define GAME_OVER_COLOR {255, 0, 0}
 #define GAME_OVER_SPACING 25
 #define GAME_OVER_SUBCAPTION_SIZE 32
+
+#ifdef __wii__
+#define GAME_OVER_SUBCAPTION_TEXT "Press the + button to restart"
+#else
 #define GAME_OVER_SUBCAPTION_TEXT "Press the Enter key to restart"
+#endif
 
 #endif
