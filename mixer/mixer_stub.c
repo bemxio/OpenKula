f44fc9c60984862d95c1771d8adedf85f9422950
bemxio <bemxiov@protonmail.com>
2026-02-02 07:14:58 +0100

    add optimization flags

--- mixer/mixer_stub.c
+++ mixer/mixer_stub.c
@@ -2,7 +2,7 @@
 
 void Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunkSize) { return; }
 
-Mix_Music* Mix_LoadMUS(const char* file) { return (void*)NULL; }
+Mix_Music* Mix_LoadMUS(const char* file) { return NULL; }
 void Mix_PlayMusic(Mix_Music* music, int loops) { return; }
 
 void Mix_SetMusicPosition(double position) { return; }
