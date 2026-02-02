bc161bd59ca635cfa8f1f1823e63fcb5a60a0b3a
bemxio <bemxiov@protonmail.com>
2026-02-02 06:56:07 +0100

    add mixer to makefiles

--- mixer/mixer_stub.c
+++ mixer/mixer_stub.c
@@ -1,14 +1,14 @@
-#include "mixer_stub.h"
+#include "mixer.h"
 
-void Mix_OpenAudio(int, unsigned short, int, int) { return; }
+void Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunkSize) { return; }
 
-Mix_Music* Mix_LoadMUS(const char*) { return (void*)0; }
-void Mix_PlayMusic(Mix_Music*, int) { return; }
+Mix_Music* Mix_LoadMUS(const char* file) { return (void*)NULL; }
+void Mix_PlayMusic(Mix_Music* music, int loops) { return; }
 
-void Mix_SetMusicPosition(double) { return; }
+void Mix_SetMusicPosition(double position) { return; }
 void Mix_ResumeMusic() { return; }
 void Mix_PauseMusic() { return; }
 
-void Mix_FreeMusic(void*) { return; }
+void Mix_FreeMusic(Mix_Music* music) { return; }
 void Mix_CloseAudio() { return; }
 void Mix_Quit() { return; }
